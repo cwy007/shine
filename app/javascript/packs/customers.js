7584377f878189e470ed9480797194b0448f9d0b
cwy007 <chanweiyan007@gmail.com>
2018-01-17 22:06:38 +0800

    configuring angular's router for user navigation

--- app/javascript/packs/customers.js
+++ app/javascript/packs/customers.js
@@ -1,12 +1,24 @@
 import "polyfills";
 import "babel-polyfill";
 
-import { NgModule                } from "@angular/core";
+import { Component, NgModule     } from "@angular/core";
 import { BrowserModule           } from "@angular/platform-browser";
 import { FormsModule             } from "@angular/forms";
 import { platformBrowserDynamic  } from "@angular/platform-browser-dynamic";
 import { HttpModule              } from "@angular/http";
-import { CustomerSearchComponent } from "CustomerSearchComponent";
+import { RouterModule            } from "@angular/router";
+
+import { CustomerSearchComponent  } from "CustomerSearchComponent";
+import { CustomerDetailsComponent } from "CustomerDetailsComponent";
+
+var AppComponent = Component({
+  selector: "shine-customers-app",
+  template: "<router-outlet></router-outlet>"
+}).Class({
+  constructor: [
+    function() {}
+  ]
+});
 
 var RESULTS = [
   {
@@ -46,10 +58,22 @@ var RESULTS = [
   },
 ];
 
+var routing = RouterModule.forRoot(
+[
+  {
+    path: "",
+    component: CustomerSearchComponent
+  },
+  {
+    path: ":id",
+    component: CustomerDetailsComponent
+  }
+]);
+
 var CustomerAppModule = NgModule({
-  imports:      [ BrowserModule, FormsModule, HttpModule ],
-  declarations: [ CustomerSearchComponent ],
-  bootstrap:    [ CustomerSearchComponent ]
+  imports:      [ BrowserModule, FormsModule, HttpModule, routing ],
+  declarations: [ CustomerSearchComponent, CustomerDetailsComponent, AppComponent ],
+  bootstrap:    [ AppComponent ]
 }).Class({
   constructor: function() {}
 });
