b6a23061c7f72b20e64f2eccff48689ee9b09455
cwy007 <chanweiyan007@gmail.com>
2018-01-20 19:24:58 +0800

    sharing data between components

--- app/javascript/CustomerDetailsComponent/AddressComponent.ts
+++ app/javascript/CustomerDetailsComponent/AddressComponent.ts
@@ -3,11 +3,18 @@ import   template    from "./AddressComponent.html";
 
 var AddressComponent = Component({
   selector: "shine-address",
+  // allowing set via binding
+  inputs: [
+    "address",
+    "addressType"
+  ],
   template: template
 }).Class({
   constructor: [
     function() {
-
+      // component properties initial value
+      this.address     = null;
+      this.addressType = null;
     }
   ]
 })
--- app/javascript/CustomerDetailsComponent/CustomerInfoComponent.ts
+++ app/javascript/CustomerDetailsComponent/CustomerInfoComponent.ts
@@ -3,13 +3,17 @@ import   template    from "./CustomerInfoComponent.html";
 
 var CustomerInfoComponent = Component({
   selector: "shine-customer-info",
+  // CustomerInfoComponent have its own properties set by other components
+  inputs: [
+    "customer"
+  ],
   template: template
 }).Class({
   constructor: [
     function() {
-
+      this.customer = null;
     }
   ]
 })
 
-export { CustomerInfoComponent } 
+export { CustomerInfoComponent }
